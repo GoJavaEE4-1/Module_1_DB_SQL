6a8b9b458558806c1e87b5c1b07e213718b87e0c
DGreenleaf <serg.savitskiy@gmail.com>
2016-11-20 15:51:42 +0200

    inserting DATA into PROJECTS table

---
+++ src/scripts/populateDB.sql
@@ -0,0 +1,10 @@
+
+--inserting DATA into PROJECTS table
+
+insert into projects values (1, 'Wedding agency', 20000, 'DevTeam 1');
+insert into projects values (2, 'Online hotel booking', 30000, 'DevTeam 2');
+insert into projects values (3, 'Online currency converter', 5000, 'DevTeam 3');
+insert into projects values (4, 'Social advertising', 5000, 'DevTeam 4');
+insert into projects values (5, 'Flight tickets', 10000, 'DevTeam 5');
+insert into projects values (6, 'Web-banking', 30000, 'DevTeam 6');
+insert into projects values (7, 'Chinese food market', 15000, 'DevTeam 7');
