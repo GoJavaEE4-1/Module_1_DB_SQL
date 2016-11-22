607e0f5008bff181ecb9b17d8eb71a57143f10f7
art07 <artem-dubinin@yandex.ru>
2016-11-22 02:38:26 +0200

    3) To calculate the total salary of all... https://trello.com/c/t5Xa1Lz4

--- src/scripts/3.sql
+++ src/scripts/3.sql
@@ -1,4 +1,12 @@
-
--- adding column cost to table projects
-
-    alter table projects add column project_cost real;
+-- 3) To calculate the total salary of all Java developers - https://trello.com/c/t5Xa1Lz4
+	SELECT
+	"SKILLS"."SKILL",
+	SUM ("DEVELOPERS"."SALARY")
+	FROM
+	"SKILLS"
+	INNER JOIN "DEV_SKI" ON "DEV_SKI"."SKI_ID" = "SKILLS"."SKILL_ID"
+	INNER JOIN "DEVELOPERS" ON "DEV_SKI"."DEV_ID" = "DEVELOPERS"."DEVELOPERS_ID"
+	GROUP BY
+	"SKILLS"."SKILL"
+	HAVING
+	homework_1."SKILLS"."SKILL" = 'Java';
