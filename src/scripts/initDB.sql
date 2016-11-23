ca7a3f7fc63f9cb5a0b6961bd7c1c863f826a70b
DGreenleaf <serg.savitskiy@gmail.com>
2016-11-23 21:42:23 +0200

    creating developers table

--- src/scripts/initDB.sql
+++ src/scripts/initDB.sql
@@ -1,7 +1,21 @@
+
+
+     create table developers (
+         developer_id serial primary key,
+         developer_first_name character varying(15) not null,
+         developer_last_name character varying(15) not null,
+         developer_email character varying(50) not null,
+         developer_phone character varying(15) not null,
+         developer_experience integer not null,
+         project_id_fk integer references projects(project_id),
+         company_id_fk integer references companies(company_id)
+     );
+
      create table skills(
             skill_id serial primary key,
             skill_name varchar not null
     );
+
 
      create table customers(
             customer_id serial primary key,
