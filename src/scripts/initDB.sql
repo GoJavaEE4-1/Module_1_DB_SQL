59c1fb1cb207f039ae16b5922c67bf7d16a3de3c
Tiger <Shramkot@ukr.net>
2016-11-23 21:35:17 +0200

    table customers

--- src/scripts/initDB.sql
+++ src/scripts/initDB.sql
@@ -2,3 +2,8 @@
             skill_id serial primary key,
             skill_name varchar not null
     );
+
+     create table customers(
+            customer_id serial primary key,
+            customer_name varchar not null
+    );
