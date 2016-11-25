7188cdebd29e53624d39709c6a2d16c84eed157d
Tiger <Shramkot@ukr.net>
2016-11-25 02:01:43 +0200

    Вычислить, среднюю ЗП программистов в проекте, который приносит наименьшую прибыль.

---
+++ src/scripts/6.sql
@@ -0,0 +1,7 @@
+create view avarage as
+select projects.project_profit, projects.project_id, developers.developer_last_name, developers.salary
+from projects, developers
+where projects.project_id=developers.project_id_fk and project_profit=
+(select min(project_profit) from projects);
+
+select avg(salary) from avarage;
