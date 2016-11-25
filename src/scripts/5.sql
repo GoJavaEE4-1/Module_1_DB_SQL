b15e81a3d30d7a30fe8b502733975843926b60d1
DGreenleaf <serg.savitskiy@gmail.com>
2016-11-26 01:23:58 +0200

    doing 5.sql - finally

--- src/scripts/5.sql
+++ src/scripts/5.sql
@@ -1,13 +1,9 @@
 -- 5) To find a client (customer) that brings the least profit of the company (the company) for each of the companies - https://trello.com/c/hKPUsy6t
-	SELECT
-	"COMPANIES"."COMPANY_ID",
-	"COMPANIES"."NAME",
-	"PROJECTS"."PROJECT_NAME",
-	"PROJECTS"."COST",
-	"CUSTOMERS"."NAME"
-	FROM
-	"COMPANIES"
-	INNER JOIN "COM_PRO" ON "COM_PRO"."COM_ID" = "COMPANIES"."COMPANY_ID"
-	INNER JOIN "PROJECTS" ON "COM_PRO"."PRO_ID" = "PROJECTS"."PROJECT_ID"
-	INNER JOIN "CUSTOMERS" ON "PROJECTS"."CUSTOMER_ID" = "CUSTOMERS"."CUSTOMER_ID"
-	ORDER BY "COMPANIES"."NAME", "PROJECTS"."COST";
+
+
+
+	select customer_first_name, company_name, min (project_profit) from customers c
+    inner join projects p on c.customer_id = p.customer_id_fk
+    left join companies on companies.company_id = p.company_id_fk
+    group by customer_first_name, company_name
+    order by min (project_profit)
