dbbaf3e7f7123351f911ea714f06fd3f3bb2a3fb
Tiger <Shramkot@ukr.net>
2016-11-25 02:09:36 +0200

    avarage salary for the project with  lowest profit

--- src/scripts/6.sql
+++ src/scripts/6.sql
@@ -1,5 +1,5 @@
 create view avarage as
-select projects.project_profit, projects.project_id, developers.developer_last_name, developers.salary
+select projects.project_profit, projects.project_id, developers.salary
 from projects, developers
 where projects.project_id=developers.project_id_fk and project_profit=
 (select min(project_profit) from projects);
