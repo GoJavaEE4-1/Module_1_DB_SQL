7bd95c82cf8bbbcf08e2f9807766a5bf8ed27761
art07 <artem-dubinin@yandex.ru>
2016-11-27 01:42:54 +0200

    total salary of all Java developers

--- src/scripts/3.sql
+++ src/scripts/3.sql
@@ -1,12 +1,11 @@
 -- 3) To calculate the total salary of all Java developers - https://trello.com/c/t5Xa1Lz4
-	SELECT
-	"SKILLS"."SKILL",
-	SUM ("DEVELOPERS"."SALARY")
-	FROM
-	"SKILLS"
-	INNER JOIN "DEV_SKI" ON "DEV_SKI"."SKI_ID" = "SKILLS"."SKILL_ID"
-	INNER JOIN "DEVELOPERS" ON "DEV_SKI"."DEV_ID" = "DEVELOPERS"."DEVELOPERS_ID"
-	GROUP BY
-	"SKILLS"."SKILL"
-	HAVING
-	homework_1."SKILLS"."SKILL" = 'Java';
+select
+	skills.skill,
+	sum( developers.salary )
+from
+	skills inner join dev_ski on dev_ski.ski_id = skills.skill_id
+	inner join developers on dev_ski.dev_id = developers.developer_id
+where
+	skills.skill = 'Java'
+group by
+	skills.skill
