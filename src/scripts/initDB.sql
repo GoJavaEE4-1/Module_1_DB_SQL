cf19f69e586fd8f297b8e214bcf8b5238eed51cb
DGreenleaf <serg.savitskiy@gmail.com>
2016-11-27 22:16:11 +0200

    total inserting and fixing logic

--- src/scripts/initDB.sql
+++ src/scripts/initDB.sql
@@ -1,44 +1,52 @@
-     create table skills(
-            skill_id serial primary key,
-            skill_name varchar not null
-    );
+create table skills(
+    skill_id serial primary key,
+    skill_name varchar not null
+);
 
-     create table customers(
-            customer_id serial primary key,
-            customer_name varchar not null
-    );
+create table customers(
+    customer_id serial primary key,
+    customer_name varchar not null
+);
 
-     create table companies (
-			company_id int not null,
-			name varchar(30) not null,
-			staff int null,
-			constraint companies_pk primary key (company_id)
-	);
+create table companies (
+		company_id serial not null,
+		company_name varchar(30) not null,
+		company_staff int not null,
+		constraint companies_pk primary key (company_id)
+);
 
-    create table projects (
-             project_id serial primary key,
-             project_name character varying(30) not null,
-             project_description text,
-             project_profit numeric,
-             company_id_fk integer references companies(company_id),
-             customer_id_fk integer references customers(customer_id)
-    );
+create table projects (
+    project_id serial primary key,
+    project_name character varying(30) not null,
+    project_description text,
+    project_profit numeric,
+    company_id_fk integer references companies(company_id),
+    customer_id_fk integer references customers(customer_id)
+);
 
-    create table developers (
-             developer_id serial primary key,
-             developer_first_name character varying(15) not null,
-             developer_last_name character varying(15) not null,
-             developer_email character varying(50) not null,
-             developer_phone character varying(15) not null,
-             project_id_fk integer references projects(project_id),
-             company_id_fk integer references companies(company_id)
-    );
+create table developers (
+    developer_id serial primary key,
+    developer_first_name character varying(15) not null,
+    developer_last_name character varying(15) not null,
+    developer_email character varying(50) not null,
+    developer_phone character varying(15) not null,
+    project_id_fk integer references projects(project_id),
+    company_id_fk integer references companies(company_id)
+);
 
-    create table dev_skills(
-            dev_id integer not null,
-            sk_id integer not null,
-            constraint dev_id_fk foreign key (dev_id) references developers (developer_id) on delete no action on update no action,
-            constraint sk_id_fk foreign key (sk_id) references skills (skill_id) on delete no action on update no action
-    );
+-- create table dev_skills(
+--     developer_id integer not null,
+--     skill_id integer not null,
+--     constraint developer_id_fk foreign key (developer_id) references developers (developer_id) on delete no action on update no action,
+--     constraint skill_id_fk foreign key (skill_id) references skills (skill_id) on delete no action on update no action
+-- );
+
+create table dev_skills(
+    developer_id integer not null,
+    skill_id integer not null,
+    developer_id_fk integer references developers (developer_id) on delete no action on update no action,
+    skill_id_fk integer references skills (skill_id) on delete no action on update no action
+);
+
 
     alter table projects add production_date date;
--- src/scripts/populateDB.sql
+++ src/scripts/populateDB.sql
@@ -1,49 +1,89 @@
+--inserting DATA into SKILLS table
 
---inserting DATA into DEVELOPERS table
+insert into skills (skill_name) values ('Java');
+insert into skills (skill_name) values ('C++');
+insert into skills (skill_name) values ('Fortran');
+insert into skills (skill_name) values ('Basic');
+insert into skills (skill_name) values ('ObjectiveC');
+insert into skills (skill_name) values ('Pascal');
+insert into skills (skill_name) values ('C');
+insert into skills (skill_name) values ('Python');
+insert into skills (skill_name) values ('PHP');
+insert into skills (skill_name) values ('Ruby');
 
-insert into developers (developer_first_name, developer_last_name, developer_email, developer_phone, project_id_fk, company_id_fk)
-values ('Ivan', 'Virsky', 'devmon@gmail.com', '+380224563454', 2, 1);
-insert into developers (developer_first_name, developer_last_name, developer_email, developer_phone, project_id_fk, company_id_fk)
-values ('John', 'Table', 'info@softlab.com', '+380231528414', 4, 3);
-insert into developers (developer_first_name, developer_last_name, developer_email, developer_phone, project_id_fk, company_id_fk)
-values ('Potty', 'Mist', 'ss@gmail.com', '+380294626396', 6, 6);
-insert into developers (developer_first_name, developer_last_name, developer_email, developer_phone, project_id_fk, company_id_fk)
-values ('David', 'Dorts', 'it.zone@zoneit.com', '+380270534522', 1, 7);
-insert into developers (developer_first_name, developer_last_name, developer_email, developer_phone, project_id_fk, company_id_fk)
-values ('Valiant', 'Miserty', 'it.zone@zoneit.com', '+380270534522', 2, 2);
-insert into developers (developer_first_name, developer_last_name, developer_email, developer_phone, project_id_fk, company_id_fk)
-values ('Jenny', 'Telly', 'it.zone@zoneit.com', '+380270534522', 1, 5);
-insert into developers (developer_first_name, developer_last_name, developer_email, developer_phone, project_id_fk, company_id_fk)
-values ('Roger', 'Brown', 'it.zone@zoneit.com', '+380270534522', 4, 4);
+--inserting DATA into CUSTOMERS table
 
+insert into customers (customer_name) values ('John Derreck');
+insert into customers (customer_name) values ('Merry Jane');
+insert into customers (customer_name) values ('Lisa Huvir');
+insert into customers (customer_name) values ('Sobby Sorrotski');
+insert into customers (customer_name) values ('Nanny Debili');
 
+--inserting DATA into COMPANIES table
+
+insert into companies (company_name, company_staff) values ('epam', 4500);
+insert into companies (company_name, company_staff) values ('softserve', 4171);
+insert into companies (company_name, company_staff) values ('luxoft', 3735);
+insert into companies (company_name, company_staff) values ('globallogic', 2751);
+insert into companies (company_name, company_staff) values ('ciklum', 2500);
+insert into companies (company_name, company_staff) values ('nix solutions ltd.', 1500);
 
 --inserting DATA into PROJECTS table
 
-insert into projects (project_name, project_description, project_profit, company_id_fk, customer_id_fk)
+insert into projects
+(project_name, project_description, project_profit, company_id_fk, customer_id_fk)
 values ('Wedding agency', 'Some project description', 20000, 1, 2);
-insert into projects (project_name, project_description, project_profit, company_id_fk, customer_id_fk)
+insert into projects
+(project_name, project_description, project_profit, company_id_fk, customer_id_fk)
 values ('Online hotel booking',  'Some project description', 30000, 2, 3);
-insert into projects (project_name, project_description, project_profit, company_id_fk, customer_id_fk)
+insert into projects
+(project_name, project_description, project_profit, company_id_fk, customer_id_fk)
 values ('Online currency converter',  'Some project description', 5000, 4, 2);
-insert into projects (project_name, project_description, project_profit, company_id_fk, customer_id_fk)
+insert into projects
+(project_name, project_description, project_profit, company_id_fk, customer_id_fk)
 values ('Social advertising',  'Some project description', 5000, 1, 3);
-insert into projects (project_name, project_description, project_profit, company_id_fk, customer_id_fk)
+insert into projects
+(project_name, project_description, project_profit, company_id_fk, customer_id_fk)
 values ('Flight tickets',  'Some project description', 10000, 3, 4);
-insert into projects (project_name, project_description, project_profit, company_id_fk, customer_id_fk)
+insert into projects
+(project_name, project_description, project_profit, company_id_fk, customer_id_fk)
 values ('Web-banking',  'Some project description', 30000, 4, 2);
-insert into projects (project_name, project_description, project_profit, company_id_fk, customer_id_fk)
+insert into projects
+(project_name, project_description, project_profit, company_id_fk, customer_id_fk)
 values ('Chinese food market',  'Some project description', 15000, 5, 3);
 
+--inserting DATA into DEVELOPERS table
+
+insert into developers
+(developer_first_name, developer_last_name, developer_email,
+developer_phone, project_id_fk, company_id_fk)
+values ('Ivan', 'Virsky', 'devmon@gmail.com', '+380224563454', 2, 1);
+insert into developers
+(developer_first_name, developer_last_name, developer_email,
+developer_phone, project_id_fk, company_id_fk)
+values ('John', 'Table', 'info@softlab.com', '+380231528414', 4, 3);
+insert into developers
+(developer_first_name, developer_last_name, developer_email,
+developer_phone, project_id_fk, company_id_fk)
+values ('Potty', 'Mist', 'ss@gmail.com', '+380294626396', 6, 6);
+insert into developers
+(developer_first_name, developer_last_name, developer_email,
+developer_phone, project_id_fk, company_id_fk)
+values ('David', 'Dorts', 'it.zone@zoneit.com', '+380270534522', 1, 3);
+insert into developers
+(developer_first_name, developer_last_name, developer_email,
+developer_phone, project_id_fk, company_id_fk)
+values ('Valiant', 'Miserty', 'it.zone@zoneit.com', '+380270534522', 2, 2);
+insert into developers
+(developer_first_name, developer_last_name, developer_email,
+developer_phone, project_id_fk, company_id_fk)
+values ('Jenny', 'Telly', 'it.zone@zoneit.com', '+380270534522', 1, 5);
+insert into developers
+(developer_first_name, developer_last_name, developer_email,
+developer_phone, project_id_fk, company_id_fk)
+values ('Roger', 'Brown', 'it.zone@zoneit.com', '+380270534522', 4, 4);
 
 
---inserting DATA into COMPANIES table
-insert into companies (company_id,"name",staff) values (0,'epam',4500);
-insert into companies (company_id,"name",staff) values (1,'softserve',4171);
-insert into companies (company_id,"name",staff) values (2,'luxoft',3735);
-insert into companies (company_id,"name",staff) values (3,'globallogic',2751);
-insert into companies (company_id,"name",staff) values (4,'ciklum',2500);
-insert into companies (company_id,"name",staff) values (5,'nix solutions ltd.',1500);
 
 
 
